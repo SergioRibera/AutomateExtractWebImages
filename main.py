06b095d7bb92a35a24d2d298d3607a3574f786c7
SergioRibera <sergioalejandroriberacosta@gmail.com>
2021-09-03 18:27:44 +0200

    Add: pillow requirement for convert to pdf, and add cookie compatibilitie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #!env/python3
 
-from types import prepare_class
 import requests, os, sys
 from tqdm import tqdm
 from bs4 import BeautifulSoup as bs
@@ -20,11 +19,11 @@ def is_valid(url):
     parsed = urlparse(url)
     return bool(parsed.netloc) and bool(parsed.scheme)
 
-def get_all_images(url):
+def get_all_images(url, cookies):
     """
     Returns all image URLs on a single `url`
     """
-    soup = bs(requests.get(url).content, "html.parser")
+    soup = bs(requests.get(url, cookies=cookies).content, "html.parser")
     urls = []
     for img in tqdm(soup.find_all("img"), "Extracting images"):
         img_url = img.attrs.get("src")
@@ -43,7 +42,7 @@ def get_all_images(url):
             urls.append(img_url)
     return urls
 
-def download(url, pathname):
+def download(url, pathname, enable_enumeration, index, cookies):
     """
     Downloads a file given an URL and puts it in the folder `pathname`
     """
@@ -51,11 +50,13 @@ def download(url, pathname):
     if not os.path.isdir(pathname):
         os.makedirs(pathname)
     # download the body of response by chunk, not immediately
-    response = requests.get(url, stream=True)
+    response = requests.get(url, stream=True, cookies=cookies)
     # get the total file size
     file_size = int(response.headers.get("Content-Length", 0))
     # get the file name
     filename = os.path.join(pathname, url.split("/")[-1])
+    if enable_enumeration == True:
+        filename = os.path.join(pathname, str(index) + "." + url.split(".")[-1])
     # progress bar, changing the unit to bytes instead of iteration (default by tqdm)
     progress = tqdm(response.iter_content(1024), f"Downloading {filename}", total=file_size, unit="B", unit_scale=True, unit_divisor=1024)
     with open(filename, "wb") as f:
@@ -68,12 +69,13 @@ def download(url, pathname):
 def help():
     print("----------- Help Guide --------------")
     print("\n python ./main.py [OPTIONS]")
-    print("\n\n Options:")
-    print("\t--url <url>                This is the url where download all images")
-    print("\t--path <path>              This is the directory where download all images")
-    print("\t--cookies <cookies>        Cookies of session to access into web")
-    print("\t--pdf                      Enable generate PDF from all images downloaded")
-    print("\t--enumerate                Enumerate images from 0..n")
+    print("\n\nOptions:")
+    print("\tOPTION                 NECESARY         DESCRIPTION")
+    print("\t--url <url>               *             This is the url where download all images")
+    print("\t--path <path>             *             This is the directory where download all images")
+    print("\t--cookies <cookies>                     Cookies of session to access into web")
+    print("\t--pdf                                   Enable generate PDF from all images downloaded")
+    print("\t--no-enumerate                          Disable enumeration images from 0..n")
     # print("\t--ignore [name name ...]   List of images names to ignore on download (Regex are valid)\n\n")
     pass
 
@@ -105,15 +107,20 @@ if __name__ == "__main__":
         if not stop:
             url = process_args(args, "url", "str")
             path = process_args(args, "path", "str")
+            cookies = process_args(args, "cookies", "str")
             enable_pdf = process_args(args, "pdf", "bool", defaultValue=False)
-            enable_enumerate = process_args(args, "enumerate", "bool", defaultValue=False)
+            enable_enumerate = not process_args(args, "no-enumerate", "bool", defaultValue=False)
             ignore_list = []
             if not url == "":
                 if not path == "":
-                    imgs = get_all_images(url)
+                    imgs = get_all_images(url, cookies)
+                    i = 0
                     for img in imgs:
                         # for each image, download it
-                        download(img, path)
+                        download(img, path, enable_enumerate, i, cookies)
+                        i = i + 1
+                    if enable_pdf == True:
+                        pass
                 else:
                     print_fail("Path is empty")
             else:
